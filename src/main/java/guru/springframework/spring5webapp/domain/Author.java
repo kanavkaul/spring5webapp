cb1a2919a7b40d96bcb2058e91bc5f4c9c5fcac6
kanavkaul <kanavkaul@gmail.com>
2023-03-11 17:40:53 -0600

    Got thymeleaf and h2 db working. Added application.yaml and printed the results using a template file.

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -42,7 +42,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -44,12 +44,10 @@ public class Book {
     }
 
     @Override
-    public String toString() {
-        return "Book{" +
+    public String toString() {        return "Book{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
